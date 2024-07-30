df7ad8261572b420e256ee9b14f932b57876b989
jayraj175coder <jayrajsanas175@gmail.com>
2024-07-30 10:33:32 +0530

    backEnd

---
+++ controllers/todoController.js
@@ -0,0 +1,45 @@
+const Todo = require('../models/Todo');
+
+// Get all todos
+exports.getTodos = async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Create a new todo
+exports.createTodo = async (req, res) => {
+  const newTodo = new Todo({
+    text: req.body.text,
+  });
+
+  try {
+    const savedTodo = await newTodo.save();
+    res.status(201).json(savedTodo);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// Update a todo
+exports.updateTodo = async (req, res) => {
+  try {
+    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedTodo);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// Delete a todo
+exports.deleteTodo = async (req, res) => {
+  try {
+    await Todo.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Todo deleted' });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
